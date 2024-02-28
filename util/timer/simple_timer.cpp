d85413cfb4d56ad2f4752e5a772096f9281ac429
Loris Lucido <loris.lucido@eviden.com>
2024-02-28 19:09:18 +0100

    correct min/max/avg on divergent path

--- util/timer/simple_timer.cpp
+++ util/timer/simple_timer.cpp
@@ -4,23 +4,32 @@
 #include <map>
 #include <algorithm>
 
-#ifdef _CUDA
-#include "nvToolsExt.h"
-#elif _HIP
+#ifdef CUDA
+#include <nvtx3/nvToolsExt.h>
+#elif HIP
 #include <roctracer/roctx.h>
 #endif
 
 #include "simple_timer.h"
 
+// each rank stores their own timers
 typedef struct {
   std::chrono::steady_clock::time_point begin;
   int color;
-  double total; // ns
-} entry;
+  double total;
+} timer_entry;
 
-std::map<std::string, entry> timers;
+std::map<std::string, timer_entry> timers;
 
-#ifdef _CUDA
+// rank 0 gather results and compute min/max/avg for all ranks
+typedef struct {
+  double min;
+  double max;
+  double sum;
+  int num;
+} rank_entry;
+
+#ifdef CUDA
 const uint32_t colors[] = { 0xf24826, 0xd17c3c, 0xd3b632, 0x31c692, 0x4123af,
                             0x7c51bc, 0xc317ea, 0xc11b3f, 0xbf931c, 0xffe949,
                             0x6ffcf7, 0x38a8b5, 0x5a1fad, 0xf268ad, 0xfc2376,
@@ -36,16 +45,18 @@ void tstart(const char* name){
   std::string s = std::string(name);
   auto now = std::chrono::steady_clock::now();
   // init total to 0 at first insertion
-  auto ret = timers.try_emplace(s,entry{now,0,0.0});
+  auto ret = timers.try_emplace(s,timer_entry{now,0,0.0});
   bool new_entry = ret.second;
   if(new_entry) {
-#ifdef _CUDA
+#ifdef CUDA
     timers[s].color = (color_counter++)%num_colors;
+#else
+    // TODO: roctx colors?
 #endif
   } else {
     timers[s].begin = now;
   }
-#ifdef _CUDA
+#ifdef CUDA
   int color_id = timers[s].color;
   nvtxEventAttributes_t eventAttrib = {0};
   eventAttrib.version = NVTX_VERSION;
@@ -55,7 +66,8 @@ void tstart(const char* name){
   eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
   eventAttrib.message.ascii = name;
   nvtxRangePushEx(&eventAttrib);
-#elif _HIP
+#elif HIP
+  // TODO: any roctx colors?
   roctxRangePush(name);
 #endif
 }
@@ -65,9 +77,9 @@ void tstop(const char* name){
   auto diff = now - timers[name].begin;
   double duration = std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count();
   timers[name].total += duration;
-#ifdef _CUDA
+#ifdef CUDA
   nvtxRangePop();
-#elif _HIP
+#elif HIP
   roctxRangePop();
 #endif
 }
@@ -76,139 +88,88 @@ inline double sec(double nano){
   return nano / 1000000000.0;
 }
 
-void min_func(void *a, void *b, int *len, MPI_Datatype *dptr){
-  double *in = (double*)a;
-  double *inout = (double*)b;
-  if(*in > 0)
-    *inout = *in < *inout ? *in : *inout;
-}
-
-void max_func(void *a, void *b, int *len, MPI_Datatype *dptr){
-  double *in = (double*)a;
-  double *inout = (double*)b;
-  if(*in > 0)
-    *inout = *in > *inout ? *in : *inout;
-}
-
-void sum_func(void *a, void *b, int *len, MPI_Datatype *dptr){
-  double *in = (double*)a;
-  double *inout = (double*)b;
-  if(*in > 0)
-    *inout += *in;
-}
-
+// this needs to be called by all ranks
 void tprint(){
+  MPI_Barrier(MPI_COMM_WORLD);
   auto begin = std::chrono::steady_clock::now();
 
-  int rank, size;
+  // rank 0 gather results and compute min/max/avg for all ranks
+  std::map<std::string, rank_entry> stats;
+
+  int rank, nranks;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_size(MPI_COMM_WORLD, &nranks);
 
-  // get timers not present in rank 0
+  // fetch timers to rank 0, handling cases of rank-divergent path
   if (rank == 0){
-    for (int r=1; r<size; ++r){
-      int n;
-      MPI_Recv(&n, 1, MPI_INT, r, 42, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      int *sizes = new int[n];
-      MPI_Recv(sizes, n, MPI_INT, r, 43, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      int max_length = *std::max_element(sizes, sizes+n);
+    // init min/max/avg stats with rank 0 own timer values
+    for (auto t: timers) {
+      double total = t.second.total;
+      stats.emplace(t.first, rank_entry{total,total,total,1});
+    }
+    for (int r=1; r<nranks; ++r){
+      int ntimers;
+      MPI_Recv(&ntimers, 1, MPI_INT, r, 42, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      int *timer_name_lengths = new int[ntimers];
+      MPI_Recv(timer_name_lengths, ntimers, MPI_INT, r, 43, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      int max_length = *std::max_element(timer_name_lengths, timer_name_lengths+ntimers);
       char *name = new char[max_length];
-      for (int i=0; i<n; ++i){
-        MPI_Recv(name, sizes[i], MPI_CHAR, r, 44, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      for (int i=0; i<ntimers; ++i){
+        double total;
+        MPI_Recv(name, timer_name_lengths[i], MPI_CHAR, r, 44, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&total, 1, MPI_DOUBLE, r, 45, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        entry e;
-        e.total = -2000000;
-        timers.try_emplace(std::string(name), e);
+        std::string s = std::string(name);
+        if(!stats.try_emplace(s, rank_entry{total,total,total,1}).second) { // already present, we recompute stats
+          stats[s].min = stats[s].min < total ? stats[s].min : total;
+          stats[s].max = stats[s].max > total ? stats[s].max : total;
+          stats[s].sum += total;
+          ++stats[s].num;
+        } // else: a new entry is added with this rank's stats as a starting point
       }
       delete [] name;
-      delete [] sizes;
+      delete [] timer_name_lengths;
     }
   }
 
-  // send timers to rank 0 if rank 0 doesn't have it
+  // send timers to rank 0
   if (rank != 0){
-    int n = timers.size();
-    int *sizes = new int[n];
+    int ntimers = timers.size();
+    int *timer_name_lengths = new int[ntimers];
     int i=0;
     for (auto t: timers) {
       int length = t.first.length()+1;
-      sizes[i++] = length;
+      timer_name_lengths[i++] = length;
     }
-    MPI_Send(&n, 1, MPI_INT, 0, 42, MPI_COMM_WORLD);
-    MPI_Send(sizes, n, MPI_INT, 0, 43, MPI_COMM_WORLD);
-    delete [] sizes;
+    MPI_Send(&ntimers, 1, MPI_INT, 0, 42, MPI_COMM_WORLD);
+    MPI_Send(timer_name_lengths, ntimers, MPI_INT, 0, 43, MPI_COMM_WORLD);
     for (auto t: timers) {
       const char *name = t.first.c_str();
       int length = t.first.length()+1;
       MPI_Send(name, length, MPI_CHAR, 0, 44, MPI_COMM_WORLD);
+      MPI_Send(&t.second.total, 1, MPI_DOUBLE, 0, 45, MPI_COMM_WORLD);
     }
+    delete [] timer_name_lengths;
   }
-
-  // broadcast to every one
-  if (rank == 0){
-    int n = timers.size();
-    int *sizes = new int[n];
-    int i=0;
-    for (auto t: timers) {
-      int length = t.first.length()+1;
-      sizes[i++] = length;
-    }
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(sizes, n, MPI_INT, 0, MPI_COMM_WORLD);
-    delete [] sizes;
-    for (auto t: timers) {
-      const char *name = t.first.c_str();
-      int length = t.first.length()+1;
-      MPI_Bcast((char*)name, length, MPI_CHAR, 0, MPI_COMM_WORLD);
-    }
-  }else{
-    int n;
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    int *sizes = new int[n];
-    MPI_Bcast(sizes, n, MPI_INT, 0, MPI_COMM_WORLD);
-    int max_length = *std::max_element(sizes, sizes+n);
-    char *name = new char[max_length];
-    for (int i=0; i<n; ++i){
-      MPI_Bcast(name, sizes[i], MPI_CHAR, 0, MPI_COMM_WORLD);
-
-      entry e;
-      e.total = -3000000;
-      timers.try_emplace(std::string(name), e);
-    }
-    delete [] name;
-    delete [] sizes;
-  }
-
-  MPI_Op min_op, max_op, sum_op;
-  MPI_Op_create(min_func, 1, &min_op);
-  MPI_Op_create(max_func, 1, &max_op);
-  MPI_Op_create(sum_func, 1, &sum_op);
 
   int length=-1;
   if (rank == 0) {
     // find max timer name length
-    for (auto t: timers) {
+    for (auto t: stats) {
       int l = t.first.length();
       length = l > length ? l : length;
     }
     printf("| %*s | %10s | %10s | %10s |\n", length, "name", "min(s)", "max(s)", "avg(s)");
-  }
 
-  for (auto t: timers) {
-    std::string name = t.first;
+    for (auto t: stats) {
+      std::string name = t.first;
 
-    double total = sec(t.second.total);
-    double min,max,avg;
-    MPI_Reduce(&total, &min, 1, MPI_DOUBLE, min_op, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&total, &max, 1, MPI_DOUBLE, max_op, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&total, &avg, 1, MPI_DOUBLE, sum_op, 0, MPI_COMM_WORLD);
-    avg /= (double)size;
-    if (rank == 0) {
+      double min = sec(t.second.min);
+      double max = sec(t.second.max);
+      double avg = sec(t.second.sum/(double)t.second.num);
       printf("| %*s | %10lf | %10lf | %10lf |\n", length, name.c_str(), min, max, avg);
     }
-  }
 
-  if (rank == 0) {
     auto now = std::chrono::steady_clock::now();
     auto diff = now - begin;
     double duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count();
