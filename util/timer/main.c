a7cbd04dd7eb27c802dee1e883f4cb38b464c844
Loris Lucido <loris.lucido@eviden.com>
2024-02-28 19:09:06 +0100

    cmake build

--- util/timer/main.c
+++ util/timer/main.c
@@ -9,22 +9,25 @@ int main(){
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  tstart("toto");
-  usleep(1000000);
-  tstop("toto");
-  tstart("foo");
-  usleep(1000000);
-  tstop("foo");
+  tstart("all ranks");
+  usleep(100000*rank*0.5);
+  tstop("all ranks");
 
   if (rank==0){
     tstart("rank0");
-    usleep(10000);
+    usleep(100000);
     tstop("rank0");
   }
   if (rank==1){
     tstart("rank1___________________");
-    usleep(10000);
+    usleep(100000);
     tstop("rank1___________________");
+  }
+
+  if (rank==2 || rank==1){
+    tstart("rank1&2");
+    usleep(100000*rank);
+    tstop("rank1&2");
   }
 
   tprint();
--- util/timer/main.f90
+++ util/timer/main.f90
@@ -1,6 +1,26 @@
+! https://stackoverflow.com/questions/6931846/sleep-in-fortran/6932232#6932232
+module fortran_sleep
+
+  use, intrinsic :: iso_c_binding, only: c_int64_t
+
+  implicit none
+
+  interface
+
+     subroutine fsleep(useconds)  bind ( C, name="usleep" )
+       import
+       integer (c_int64_t), intent (in), VALUE :: useconds
+     end subroutine fsleep
+
+  end interface
+
+end module fortran_sleep
+
 program main
+  use, intrinsic :: iso_c_binding, only: c_int
   use mpi
   use simple_timer
+  use fortran_sleep
   implicit none
   integer::ierr,rank
 
@@ -8,17 +28,28 @@ program main
 
   call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr);
 
-  call tstart("toto")
-  call sleep(1)
-  call tstop("toto")
-  call tstart("foo")
-  call sleep(1)
-  call tstop("foo")
-  call tstart("foo")
-  call sleep(2)
-  call tstop("foo")
+  call tstart("all ranks")
+  call fsleep(100000/2*rank)
+  call tstop("all ranks")
+
+  if (rank.eq.0) then
+     call tstart("rank0")
+     call fsleep(100000)
+     call tstop("rank0")
+  endif
+  if (rank.eq.1) then
+     call tstart("rank1___________________")
+     call fsleep(100000)
+     call tstop("rank1___________________")
+  endif
+  if (rank.eq.2 .or. rank.eq.1) then
+     call tstart("rank1&2")
+     call fsleep(100000*rank)
+     call tstop("rank1&2")
+  endif
+
   call tprint()
 
   call MPI_Finalize(ierr)
 
-end program
+end program main
